6c0b0527def4da23dec67a46d208d7f35cfb4035
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2021-12-22 20:58:41 -0300

    Got the search coins finished. Finds it and puts it in the list one time, now have to update it on an interval.

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,51 @@
 const ctx1Cont = document.querySelector('.chart1-container')
 const ctx1 = document.querySelector('#chart1')
 const ctx2 = document.querySelector('#chart2')
+const favoriteCoins = document.querySelector('.favorite-coins')
 const coinSearchInput = document.querySelector('#coin-to-search')
 const coinSearchOptions = document.querySelector('.options')
+//Renders suggestions each time the input changes
 coinSearchInput.addEventListener('input', ()=>{renderSuggestions(coinSearchInput.value)})
+//Makes the suggestions dissapear when clicking outside the input
 coinSearchInput.addEventListener('blur', ()=>{
-    coinSearchInput.value = ''
-    renderSuggestions(' ')
+    setTimeout(()=> {
+        coinSearchInput.value = ''
+        renderSuggestions(' ')
+    }, 1000)
 })
+//Array to keep the 'favorite coins' that`s necesary to keep
+//updating each coin with it's price.
+let coinsInList = ['luna']
 
+function addCointToList(symbol) {
+    //Finds the coin to get the price to set initially 
+    //when rendering the element
+    console.log('edasd')
+    const data = coinsData.find(coin => coin.symbol == symbol)
+    const container = document.createElement('div')
+    container.classList.add('coin')
+    const name = document.createElement('p')
+    name.classList.add('coin-list-symbol')
+    name.textContent = symbol.toUpperCase()
+    const price = document.createElement('p')
+    price.classList.add('coin-list-price')
+    price.textContent = formatPrice(data.current_price)
+    container.append(name, price)
+    favoriteCoins.append(container)
+}
+function renderList() {
+
+}
+
+async function getLastPrice(ids) {
+    //'terra-luna%2Cripple'
+    const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`)
+}
 function renderSuggestions(searchValue) {
-    console.log(searchValue)
+    //empties the previous suggestions
     coinSearchOptions.innerHTML = ''
-    const filteredCoins = coinsList.filter(coin => coin.symbol.includes(searchValue))
+    const filteredCoins = coinsData.filter(coin => coin.symbol.includes(searchValue))
+    //creates the option element
     filteredCoins.forEach(coin => {
         const option = document.createElement('div')
         option.classList.add('option')
@@ -20,26 +53,32 @@ function renderSuggestions(searchValue) {
         name.textContent = coin.symbol
         const addSymbol = document.createElement('i')
         addSymbol.classList.add('fas','fa-plus')
+        addSymbol.title = 'Add to list.'
+        //if clicked, adds the coin to the 'favorite coins'
+        addSymbol.addEventListener('click', ()=>{addCointToList(coin.symbol)})
         option.append(name,addSymbol)
         coinSearchOptions.append(option)
     })
 }
-
+//fetches the main 250 coins from the CoinGecko API
 async function getCoinsList() {
     const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false')
     const data = await response.json()
     return data
 }
-let coinsList = []
+//Array to keep data about the fetched coins
+let coinsData = []
+//Used to update data regularly
+// setInterval(()=> {
+//     console.log(coinsData)
+// }, 10000)
 getCoinsList()
 .then(data=> {
-    coinsList = data
+    coinsData = data
+    console.log(data)
 })
 .catch(err => console.log(err))
 
-setTimeout(() => {
-    console.log(coinsList)
-},1000)
 const Chart = require('chart.js')
 
 let labels = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019']
@@ -66,13 +105,14 @@ let myChart = new Chart(ctx1, config)
 // resized, thats why we do this actions, we change the
 // size briefly and put it back to 50%
 
-async function getCoinData(coin) {
-    const response = await fetch('https://api.coingecko.com/api/v3/coins/terra-luna/market_chart?vs_currency=usd&days=8&interval=daily')
+async function getCoinData(id) {
+    const response = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=8&interval=daily`)
     const data = response.json()
     return data
 }
 
-const price = getCoinData()
+
+const price = getCoinData('terra-luna')
 price.then(response => {
     //console.log(response)
     const priceValues = response.prices.map(price => formatPrice(price[1]))
